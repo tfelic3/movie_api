23ecda004e0b99f66c66b21e18815826f79c82b1
Tesalon Felicien <felicien.tesalon@gmail.com>
2020-08-13 13:33:29 -0400

    Added urls for movies as well as documentation. Used POSTMAN to authenticate URLs

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-const express = require('express');
-morgan = require('morgan');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
 const app = express();
+
+const morgan = require('morgan');
+
+app.use(bodyParser.json());
 
 app.use(morgan('common'));
 app.use(express.static('public'))
 
 
-app.get('/', (req, res) => {
- let html =  `<body><p>Welcome! Please select your movie.</p></body>`;
-   res.send(html);
-  });
+let movies= 
+[
+    {name: "Trigun",
+genre: "Anime",
+director: "Shigeru Kitayama"},
+
+{name: "Outlaw Star",
+genre: "Anime",
+director:"Takehiko Ito"},
+{name: "Mobile Suit Gundam Wing",
+genre: "Anime",
+director: " Yoshiyuki Tomino"}
+];
+
+
   
   app.get('/documentation', (req, res) => {                  
     res.sendFile('public/documentation.html');
   });
     
-  app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
+  app.get('/movies', (req, res) => {
+    res.json(movies);
+  });
+
+  app.get('/movies/data', (req, res) => {
+    res.send('This is the information for all the movies you selected.');
   });
   
+  // Adds data for a new student to our list of students.
+app.post('/movies/users', (req, res) => {
+    let newMovie = req.body;
   
+    if (!newMovie.name) {
+      const message = 'Missing movie name in request body';
+      res.status(400).send(message);
+    } else {
+      newMovie.id = uuid.v4();
+      movies.push(newMovie);
+      res.status(201).send(newMovie);
+    }
+  });
+
+  app.get('/movies/users', (req, res) => {
+    res.send('Welcome to your account.');
+  });
+
+
   app.listen(8080, () =>
     console.log('Your app is listening on port 8080.')
   );

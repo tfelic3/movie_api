920ab7de44fe122fb51e711dbe79665bd4b39df5
Tesalon Felicien <felicien.tesalon@gmail.com>
2020-09-03 00:23:00 -0400

    Updated endpoints using Mongoose and used Postman for verification

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   uuid = require('uuid');
 
+// Adding Mongoose to project  
+const mongoose =require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myflixdb', { useNewUrlParser: true, useUnifiedTopology: true }). then(()=>console.log('Connected to database'))
+.catch(err => console.err('Could not connect to MongoDB...'));
+
 const app = express();
 
 const morgan = require('morgan');
@@ -11,52 +21,167 @@ app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(express.static('public'))
 
-
-let movies= 
-[
-    {name: "Trigun",
-genre: "Anime",
-director: "Shigeru Kitayama"},
-
-{name: "Outlaw Star",
-genre: "Anime",
-director:"Takehiko Ito"},
-{name: "Mobile Suit Gundam Wing",
-genre: "Anime",
-director: " Yoshiyuki Tomino"}
-];
-
-
   
   app.get('/documentation', (req, res) => {                  
-    res.sendFile('public/documentation.html');
+    res.sendFile('public/documentation.html', { root: __dirname });
+  
   });
     
   app.get('/movies', (req, res) => {
-    res.json(movies);
+    Movies.find({"Title": "Mulan"}).then((movies)=>{
+res.json(movies);
+    });
+   
+   
   });
 
-  app.get('/movies/data', (req, res) => {
-    res.send('This is the information for all the movies you selected.');
+
+app.get('/directors', (req, res) => {
+    Movies.find({"Director.Name": "Ryan Coogler"}).then((directors)=>{
+res.json(directors);
+    });
+   
+
+   
   });
-  
+
+  app.get('/movies/genres', (req, res) => {
+    Movies.find({"Genre.Name": "Action" }).then((genre)=>{
+res.json(genre);
+    });
+   
+
+   
+  });
+
+
   // Adds data for a new student to our list of students.
-app.post('/movies/users', (req, res) => {
-    let newMovie = req.body;
-  
-    if (!newMovie.name) {
-      const message = 'Missing movie name in request body';
-      res.status(400).send(message);
-    } else {
-      newMovie.id = uuid.v4();
-      movies.push(newMovie);
-      res.status(201).send(newMovie);
-    }
+
+
+ //Get info about all users
+ app.get('/users/', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+ //Get a specific user by name 
+ app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+   //Allows new users to register
+   app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday  
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
   });
 
-  app.get('/movies/users', (req, res) => {
-    res.send('Welcome to your account.');
+
+  //Allow user to update information.
+  app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+      {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      }
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if(err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
   });
+
+  //Update movie list
+  app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+       $push: { FavoriteMovies: req.params.MovieID }
+     },
+     { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+  });
+
+
+  //Deleting movie from list
+  app.delete('/users/:Username/Movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+       $pull: { FavoriteMovies: req.params.MovieID }
+     },
+     { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+  });
+
+  //Delete User from list
+  app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + ' was not found');
+        } else {
+          res.status(200).send(req.params.Username + ' was deleted.');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
+
+
 
 
   app.listen(8080, () =>
@@ -66,4 +191,4 @@ app.post('/movies/users', (req, res) => {
   app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-  });
+  })
